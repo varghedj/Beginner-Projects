cc81b56bad4aa91401abe6a58cb780e1c7ef3c80
unknown <dylanvarghese@gmail.com>
2022-01-22 08:31:28 -0500

    added 8ball beginner program

---
+++ 8ball.js
@@ -0,0 +1,54 @@
+let userName = 'Dylan';
+
+function greetUser(userName) {
+    return (userName ? `Hello, ${userName}!` : 'Hello!')
+}
+
+let userQuestion = 'Will this weekend be a three day weekend?';
+
+if (userName) {
+    console.log(`Hello, ${userName}!`);
+} else {
+    console.log('Hello!')
+}
+console.log(userQuestion);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+switch(randomNumber) {
+    case 0:
+        eightBall = 'It is ceartain';
+        break;
+
+    case 1:
+        eightBall = 'It is decidedly so';
+        break;
+
+    case 2:
+        eightBall = 'Reply hazy try again';
+        break;
+
+    case 3:
+        eightBall = 'Cannot predict now';
+        break;
+
+    case 4:
+        eightBall = 'Do not count on it';
+        break;
+
+    case 5:
+        eightBall = 'My sources say no';
+        break;
+
+    case 6:
+        eightBall = 'Outlook not so good';
+        break;
+
+    case 7:
+        eightBall = 'Signs point to yes';
+        break;
+}
+
+console.log(`The eight ball answered: ${eightBall}`);
