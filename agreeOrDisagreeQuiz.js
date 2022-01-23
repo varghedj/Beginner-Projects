5761886a689c6a41d897197039703e5cb58ca458
unknown <dylanvarghese@gmail.com>
2022-01-23 05:59:02 -0500

    beginner quiz to test if statements and such

---
+++ agreeOrDisagreeQuiz.js
@@ -0,0 +1,5 @@
+let agreeOrDisagree = (str, str2) => str === str2 ? 'You agree!' : 'You disagree!';
+
+// Uncomment the line below when you're ready to try out your function
+console.log(agreeOrDisagree("yep", "yep"))
+console.log(agreeOrDisagree('no', 'yes'))
