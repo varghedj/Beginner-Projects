b1a5589fe0ed30d640fe72b790ac9e95a79d0958
unknown <dylanvarghese@gmail.com>
2022-01-23 07:43:42 -0500

    beginner temperature converter program

---
+++ tempConverter.js
@@ -0,0 +1,18 @@
+
+// This is today's forecast in K
+let kelvin = 293;
+
+// kelvin to celsius conversion
+let celsius = kelvin - 273;
+
+// celsius to fahrenheit conversion
+let fahrenheit = celsius * (9/5) + 32;
+
+//rounds F value down to nearest whole number
+fahrenheit = Math.floor(fahrenheit);
+
+//converts C to Newton
+let Newton = celsius * (33/100);
+Newton = Math.floor(Newton);
+
+console.log(`The temperature is ${Newton} Newtons`);
