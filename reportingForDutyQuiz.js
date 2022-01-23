43b1f387c1cc49a511b907a5e25119efc007c496
unknown <dylanvarghese@gmail.com>
2022-01-23 07:48:15 -0500

    this was a quiz that dealt with easy string interpolation stuff

---
+++ reportingForDutyQuiz.js
@@ -0,0 +1,6 @@
+// Write your function here:
+let reportingForDuty = (rank, lastName) => `${rank} ${lastName} reporting for duty!`
+
+
+// Uncomment the line below when you're ready to try out your function
+console.log(reportingForDuty('Private', 'Fido')) // Should return 'Private Fido reporting for duty!'
